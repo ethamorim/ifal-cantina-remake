1b2380256f98de826a7f4e8f8277716461424461
ethamorim <ytasamorim7@gmail.com>
2023-12-24 19:14:01 -0300

    (test) add test suite for AcessoGerente

--- service/src/test/java/br/ethamorim/cantina/ifal/domain/AcessoGerenteTests.java
+++ service/src/test/java/br/ethamorim/cantina/ifal/domain/AcessoGerenteTests.java
@@ -2,10 +2,10 @@ package br.ethamorim.cantina.ifal.domain;
 
 import br.ethamorim.cantina.ifal.exceptions.EmptyParameterException;
 import br.ethamorim.cantina.ifal.exceptions.InvalidParameterException;
-import org.hibernate.exception.ConstraintViolationException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.exception.ConstraintViolationException;
 import org.hibernate.tool.schema.Action;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -13,39 +13,37 @@ import org.junit.Test;
 
 import java.util.List;
 
-public class AcessoVendedorTests {
+public class AcessoGerenteTests {
 
     private static SessionFactory sessionFactory;
 
     @BeforeClass
     public static void init() {
         sessionFactory = new Configuration()
-                .addAnnotatedClass(AcessoVendedor.class)
+                .addAnnotatedClass(AcessoGerente.class)
                 .setProperty(AvailableSettings.JAKARTA_HBM2DDL_DATABASE_ACTION, Action.SPEC_ACTION_DROP_AND_CREATE)
                 .buildSessionFactory();
     }
 
     @Test
-    public void persisteAcessoVendedor_depoisVerificaInsercao_depoisRemoveAcesso_finalmenteVerificaRemocao() {
+    public void persisteAcessoGerente_depoisVerificaInsercao_depoisRemoveAcesso_finalmenteVerificaRemocao() {
         sessionFactory.inTransaction(session -> {
-            int meta = 100;
             try {
-                var acesso = new AcessoVendedor("deisantix", "123456", meta, 0);
+                var acesso = new AcessoGerente("deisantix", "123456");
                 session.persist(acesso);
                 session.flush();
-                var acessoAdicionado = session.byNaturalId(AcessoVendedor.class)
-                        .using(AcessoVendedor_.nomeUsuario, "deisantix")
+                var acessoAdicionado = session.byNaturalId(AcessoGerente.class)
+                        .using(AcessoGerente_.nomeUsuario, "deisantix")
                         .load();
                 Assert.assertNotNull(acessoAdicionado);
                 Assert.assertEquals(acesso, acessoAdicionado);
-                Assert.assertEquals(meta, acessoAdicionado.getMetaVendas());
 
                 session.remove(acesso);
                 session.flush();
-                List<AcessoVendedor> acessos = session.createSelectionQuery("from AcessoVendedor", AcessoVendedor.class)
+                List<AcessoGerente> acessos = session.createSelectionQuery("from AcessoGerente", AcessoGerente.class)
                         .getResultList();
                 Assert.assertEquals(0, acessos.size());
-            } catch (Exception e) {
+            } catch (InvalidParameterException | EmptyParameterException e) {
                 e.printStackTrace();
             }
         });
@@ -55,8 +53,8 @@ public class AcessoVendedorTests {
     public void lancaErroAoTentarPersistirUsuariosComNomeIguais() {
         sessionFactory.inTransaction(session -> {
             try {
-                var acesso1 = new AcessoVendedor("deisantix", "123456", 50, 0);
-                var acesso2 = new AcessoVendedor("deisantix", "senha", 100, 0);
+                var acesso1 = new AcessoGerente("deisantix", "123456");
+                var acesso2 = new AcessoGerente("deisantix", "senha");
 
                 session.persist(acesso1);
                 session.persist(acesso2);
@@ -71,11 +69,11 @@ public class AcessoVendedorTests {
     public void lancaErroPorParametroNulo() {
         sessionFactory.inTransaction((session) -> {
             Assert.assertThrows(InvalidParameterException.class, () -> {
-                session.persist(new AcessoVendedor(null, "123456", 50, 0));
+                session.persist(new AcessoGerente(null, "123456"));
             });
 
             Assert.assertThrows(InvalidParameterException.class, () -> {
-               session.persist(new AcessoVendedor("deisantix", null, 50, 0));
+                session.persist(new AcessoGerente("deisantix", null));
             });
         });
     }
@@ -84,20 +82,11 @@ public class AcessoVendedorTests {
     public void lancaErroPorParametroVazio() {
         sessionFactory.inTransaction(session -> {
             Assert.assertThrows(EmptyParameterException.class, () -> {
-                session.persist(new AcessoVendedor("", "123456", 50, 0));
+                session.persist(new AcessoGerente("", "123456"));
             });
 
             Assert.assertThrows(EmptyParameterException.class, () -> {
-                session.persist(new AcessoVendedor("deisantix", "", 50, 0));
-            });
-        });
-    }
-
-    @Test
-    public void lancaErroPorParametroInvalido() {
-        sessionFactory.inTransaction(session -> {
-            Assert.assertThrows(InvalidParameterException.class, () -> {
-                session.persist(new AcessoVendedor("deisantix", "123456", 0, 0));
+                session.persist(new AcessoGerente("deisantix", ""));
             });
         });
     }
